9a6daf90fba79cf574148acb0971f51ea51d7d00
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2023-03-28 21:14:57 -0300

    finalizando projeto

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -1,5 +1,28 @@
+import styles from "./SobreMim.module.css";
+
+import PostModelo from "componentes/PostModelo";
+import fotoCapa from "assets/sobre_mim_capa.png";
+import fotoSobreMim from "assets/sobre_mim_foto.png";
+
 export default function SobreMim() {
     return (
-        <h1>Sobre mim...</h1>
+        <PostModelo
+            fotoCapa={fotoCapa}
+            titulo="Sobre mim"
+        >
+            <h3 className={styles.subtitulo}>
+                Olá, eu sou o Antônio!
+            </h3>
+
+            <img
+                src={fotoSobreMim}
+                alt="Foto do Luis Guilherme sorrindo"
+                className={styles.fotoSobreMim}
+            />
+
+            <p className={styles.paragrafo}>
+                Oi, tudo bem? Eu sou estudante de Engenharia da Computação no IMT e estudante da Alura e estou feliz de te ver por aqui.
+            </p>
+        </PostModelo>
     )
 }
